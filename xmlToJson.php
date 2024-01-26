cd40b17af7a3d18eeeb5b243b6cccc3124b3f3e8
Arunabh97 <dasarunabh036@gmail.com>
2024-01-26 12:16:48 +0530

    Added server for git pages

--- xmlToJson.php
+++ xmlToJson.php
@@ -1,5 +1,8 @@
 <?php
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: https://arunabh97.github.io");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Check if XML content is provided
